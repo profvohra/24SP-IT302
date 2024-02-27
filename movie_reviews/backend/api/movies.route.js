96934bfcbf1aded32aa1167509922938b217f5aa
Rosemina Vohra <rosemina.vohra@njit.edu>
2024-02-27 11:11:02 -0500

    Feb27

--- movie_reviews/backend/api/movies.route.js
+++ movie_reviews/backend/api/movies.route.js
@@ -5,11 +5,75 @@ import ReviewsController from "./reviews.controller.js"
 
 const router = express.Router()
 
+/**
+ * @swagger
+ * /api/v1/movies:
+ *   get:
+ *     summary: Retrieve a list of movies.
+ *     description: Retrieve a list of movies from sample_mflix MongoDB database.
+ *     tags: [movies]
+ *     parameters:
+ *       - name: page
+ *         in: query
+ *         description: Page number to return for pagination.
+ *         required: false
+ *         schema:
+ *           type: integer
+ *           maximum: 9999
+ *           format: int32
+ *     responses:
+ *       200:
+ *         description: A list of movies.
+ */
 router.route('/').get(MoviesController.apiGetMovies)
 
+router.route("/id/:id").get(MoviesController.apiGetMovieById)
+
+router.route("/ratings").get(MoviesController.apiGetRatings)
+
+/**
+ * @swagger
+ * /api/v1/movies/review:
+ *   post:
+ *     summary: Create a movie review.
+ *     description: Create a new review for a movie in the sample_mflix MongoDB database.
+ *     tags: [reviews]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               movie_id:
+ *                 type: string
+ *               review:
+ *                 type: string
+ *               user_id:
+ *                 type: string
+ *               name:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 acknowledged:
+ *                   type: boolean
+ *                   description: Acknowledgement that POST was successful.
+ *                   example: true
+ *                 insertedId:
+ *                   type: integer
+ *                   description: The new review ID.
+ *                   example: 6536e4847244a30f5b7fb0a1
+ */
 router
   .route('/review')
     .post(ReviewsController.apiPostReview)
     .put(ReviewsController.apiUpdateReview)
     .delete(ReviewsController.apiDeleteReview)
+
 export default router
--- movie_reviews/backend/server.js
+++ movie_reviews/backend/server.js
@@ -1,8 +1,47 @@
 import express from 'express'
 import cors from 'cors'
+import dotenv from "dotenv"
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+
 import movies from './api/movies.route.js'
 
+const swaggerDefinition = {
+  openapi: '3.0.0',
+  info: {
+    title: 'Express.js API for Movies and Reviews',
+    version: '1.0.0',
+    description:
+      'This is a REST API application made with Express.js. It retrieves data from MongoDB sample_mflix database.',
+   contact: {
+      name: 'Your Name',
+      url: 'https://yourwebsite.com',
+      email: 'yourucid@njit.edu',
+    },
+  },
+  servers: [
+    {
+      url: 'http://localhost:5050',
+      description: 'Development Server',
+    },
+  ],
+};
+
+const options = {
+  swaggerDefinition,
+  // Paths to files containing OpenAPI definitions
+  apis: ['./api/movies.route.js'],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+dotenv.config()
+
 const app = express()
+
+if (process.env.NODE_ENV === 'development') {
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+}
 
 app.use(cors())
 app.use(express.json())
